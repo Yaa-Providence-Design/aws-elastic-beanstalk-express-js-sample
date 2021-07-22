e9d7f5ce7b0015fd2c6ce36e62fe4b1a381fae8d
Providence Annor Asemah <baabaprovidence@gmail.com>
2021-07-22 00:01:25 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!', 'You are doing well!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/', (req, res) => res.send('You are doing well!'));
 
 app.listen(port);
